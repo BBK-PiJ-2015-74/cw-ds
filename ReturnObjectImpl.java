eee81c888d4d3193e5a25e072e4003de3730a8ae
LucieCBurgess <lucie.burgess@icloud.com>
2015-12-20 13:29:05 +0000

    Wrote test script for ReturnObject (and ReturnObjectImpl) as a result made some changes to the ReturnObjectImpl methods changed the constructors ensured that when a null test object is passed, hasError() returns true All working as it should

--- ReturnObjectImpl.java
+++ ReturnObjectImpl.java
@@ -1,46 +1,53 @@
 /**
  * Implementation of interface ReturnObject
  * @author Lburge01 BBK-PiJ-2015-75 (Lucie Burgess)
+ * ReturnObjectImpl: This class must have two constructors, each of them with only one parameter.
  */
- 
- // NB remove all comments starting with // before handing in as these are for me only
- // ReturnObjectImpl creates a ReturnObject
- 
- public class ReturnObjectImpl implements ReturnObject {
- 
- 	/**
- 	* Set up the fields in the class
- 	*/
- 	private Object myObject;
+
+public class ReturnObjectImpl implements ReturnObject {
+
+	private Object myObject;
  	private ErrorMessage myError;
  	private boolean whetherThereIsAnError;
  	
  	/**
- 	* Constructor of ReturnObjectImpl
- 	* ReturnObjectImpl: This class must have two constructors, each of them with only one parameter. 
+ 	* Constructs a ReturnObject wrapping the object 
  	* The constructor used for successful operations must receive an Object
  	* In the case of successful operations, hasError() returns false, therefore boolean whetherThereIsAnError is false
- 	* the constructor used for failed operations must receive an ErrorMessage as its only parameter
- 	* In the case of failed operations, hasError() returns true, therefore boolean whetherThereIsAnError is true
- 	* 
+ 	* @param myObject 
  	*/
- 	public ReturnObjectImpl (Object obj) {
-		myObject = obj;
+ 	public ReturnObjectImpl (Object myObject) {
+		this.myObject = myObject;
+		this.myError = ErrorMessage.NO_ERROR;
 		whetherThereIsAnError = false;
 	}
 	
-	public ReturnObjectImpl (ErrorMessage error) {
-		myError = error;
+ 	/**
+ 	* Constructs a ReturnObject consisting of an error message
+ 	* the constructor used for failed operations must receive an ErrorMessage as its only parameter
+ 	* In the case of failed operations, hasError() returns true, therefore boolean whetherThereIsAnError is true
+ 	* @param myError
+ 	*/
+ 	
+	public ReturnObjectImpl (ErrorMessage myError) {
+		this.myError = myError;
 		whetherThereIsAnError = true;
  	}
- 	
- 	/**
+	
+	/**
 	 * Returns whether there has been an error
 	 * @return whether there has been an error
+	 * Returns false if there is no error
+	 * Returns true if there IS an error
+	 * If hasError() is called on a null object, hasError() return true because this should correspond to error EMPTY_STRUCTURE
 	 */
-	//@return
+	@Override
 	public boolean hasError() {
+		if (myObject == null) {
+			return true;
+		} else {
 		return whetherThereIsAnError;
+		}
 	}
 
 	/**
@@ -48,17 +55,17 @@
 	 * This method must return NO_ERROR if and only if {@hasError} returns false.
 	 * @return the error message
 	 */
-	//@return
+	@Override
 	public ErrorMessage getError() {
 		if (whetherThereIsAnError == false) {
 			return myError = ErrorMessage.NO_ERROR;
+		} else if (myObject == null) {
+			return myError = ErrorMessage.EMPTY_STRUCTURE;
 		} else {
 			return myError;
 		}	
 	}
-	
 
-	
 	/**
 	 * Returns the object wrapped in this ReturnObject, i.e. the
 	 * result of the operation if it was successful, or null if
@@ -71,14 +78,13 @@
 	 * 
 	 * @return the return value from the method or null if there has been an error
 	 */
-	//@return
+	@Override
 	public Object getReturnValue() {
 		return myObject;
 	}
 
 }
 
-	
 
  
  	
--- ReturnObjectTest.java
+++ ReturnObjectTest.java
@@ -5,49 +5,134 @@
 
 public class ReturnObjectTest {
 
-	/**
+	/** 
 	* @param args
 	*/
-	
 	public static void main(String[] args) {
-	
-		/** 
-		* Test the return object implementation for the case where there is no error
-		* This operation should return the ErrorMessage NO_ERROR
-		* @test 
-		*/ 
 		
-		ReturnObject myTestRO1 = new ReturnObjectImpl();
-		obj = String("Lucie");
+		/**  
+		* Test what happens with a null object
+		* Test for the case that hasError() should return true when called on a null object
+		* In this case the ErrorMessage should be EMPTY_STRUCTURE
+		* @Test
+		*/	
+		ReturnObject myTestRO1 = new ReturnObjectImpl(null);
+		System.out.println("Test 1");
+		System.out.println("hasError() should return true when called on a null object");
+		if ((myTestRO1.hasError()) != true) {
+			System.out.println("There is a problem");
+		} else { 
+			System.out.println("Everything is workly correctly");
+		}
 		
-		if (myTestRO1.hasError() == false) {
-			System.out.println("There is no error and the ErrorMessage should read NO_ERROR");
-			System.out.println(myTestRO.getError());
-			System.out.println(myTestRO.getReturnObject());
-		} else {
-			System.out.println("There is an error, but there should not be, because we have entered a valid object");
-			System.out.println("Therefore something is wrong with the ReturnObjectImplementation");
+		/**
+		 * Test for what happens with a null object
+		 * Test for the case that getReturnValue() should return null on a null object 
+		 * @Test
+		 */
+		ReturnObject myTestRO2 = new ReturnObjectImpl(null);
+		System.out.println("Test 2");
+		System.out.println("getReturnValue() should return null when called on a null object");
+		if ((myTestRO2.getReturnValue()) != null) {
+			System.out.println("There is a problem");
+		} else { 
+			System.out.println("Everything is workly correctly");
+		}
+		
+		/**
+		 * Test for what happens with a null object
+		 * Test for the case that getError() should return EMPTY_STRUCTURE when called on a null object 
+		 * @Test
+		 */
+		ReturnObject myTestRO3 = new ReturnObjectImpl(null);
+		System.out.println("Test 3");
+		System.out.println("getError() should return EMPTY_STRUCTURE when called on a null object");
+		if ((myTestRO3.getError()) != ErrorMessage.EMPTY_STRUCTURE) {
+			System.out.println("There is a problem");
+		} else { 
+			System.out.println("Everything is workly correctly");
+		}
+		
+
+		/**
+		* Check that getError() method returns NO_ERROR if and only if hasError() returns false.
+		* @Test
+		*/
+		Object testObject4 = "thisIsATest";
+		ReturnObject myTestRO4 = new ReturnObjectImpl(testObject4);
+		System.out.println("Test 4");
+		System.out.println("getError() should return NO_ERROR if and only if hasError() returns false");
+		if ((myTestRO4.hasError()) == false) {
+			if ((myTestRO4.getError()) != (ErrorMessage.NO_ERROR)) {
+			System.out.println("There is a problem");
+			} else { 
+			System.out.println("Everything is workly correctly");
+			}
+		}
+		
+		/**
+		* Check that getError() method returns NO_ERROR if a valid object is passed.
+		* @Test
+		*/
+		Object testObject5 = "thisIsAnotherTest";
+		ReturnObject myTestRO5 = new ReturnObjectImpl(testObject5);
+		System.out.println("Test 5");
+		System.out.println("getError() should return NO_ERROR if a valid object is passed");
+		if ((myTestRO5.getError()) != (ErrorMessage.NO_ERROR)) {
+			System.out.println("There is a problem");
+		} else { 
+			System.out.println("Everything is workly correctly");
 		}
 		
 		
-		/** 
-		* Test the return object implementation for the case where the programmer tries to retrieve an element from an empty data structure
-		* This operation should return the ErrorMessage EMPTY_STRUCTURE
-		* @test 
-		*/ 
-	
-		ReturnObject myTestRO2 = new ReturnObjectImpl();
-		obj = null; 
-		if (myTestRO2.hasError() == false) {
-			System.out.println("There is something wrong with the ReturnObjectImplementation, because there should be an error");
-		} else {
-			System.out.println(myTestRO2.getError());
+		/**
+		 * Test that the appropriate ReturnObject is returned if there is no error, or null if there has been an error
+		 * @Test
+		 */
+		Object testObject6 = "thisIsYetAnotherTest";
+		ReturnObject myTestRO6 = new ReturnObjectImpl(testObject6);
+		System.out.println("Test 6");
+		System.out.println("getReturnValue() should return ReturnObject if a valid object is passed");
+		if (myTestRO6.getReturnValue() != testObject6) {
+			System.out.println("There is a problem");
+		} else { 
+			System.out.println("Everything is workly correctly");	
 		}
-	
-	}
-}		
-			
 		
-	
+		Object testObject7 = "thisIsAnExtraTest";
+		ReturnObject myTestRO7 = new ReturnObjectImpl(testObject7);
+		System.out.println("Test 7");
+		System.out.println("getReturnValue() should return ReturnObject if hasError() returns false i.e. if there is no error");
+		if (myTestRO7.hasError() == false) {
+			if (myTestRO7.getReturnValue() != testObject7) {
+				System.out.println("There is a problem");
+			} else { 
+				System.out.println("Everything is workly correctly");	
+			}
+		}
+		
+		Object testObject8 = null;
+		ReturnObject myTestRO8 = new ReturnObjectImpl(testObject8);
+		System.out.println("Test 8");
+		System.out.println("getReturnValue() should return null if hasError() returns true i.e. if there is an error");
+		System.out.println("The test object is null and should be " + testObject8);
+		if (myTestRO8.hasError() != true) {
+			System.out.println("Something has gone wrong");
+		}
+		if (myTestRO8.hasError() == true) {
+			if (myTestRO8.getReturnValue() != null) {
+				System.out.println("There is a problem");
+			} else { 
+				System.out.println("Everything is workly correctly");
+			}
+		}
+		
+		System.out.println("End of test file");
+
+		
+	} // end of psvm
+
+} // end of class
+
 		
 		
