2e25a82065912e1ca3f4c1782edb8dba92afd6c7
LucieCBurgess <lucie.burgess@icloud.com>
2015-12-20 16:26:54 +0000

    Started work on ListListImpl.java, implementation of interface List Currently stuck on method for adding object in the centre of the list

--- LinkedListImpl.java
+++ LinkedListImpl.java
@@ -1,41 +1,46 @@
 /**
-* This file contains an implementation of interface List, described in file List.java
-* @author Lburge01 BBK-PiJ-2015-75 (Lucie Burgess)
-*
-* all the text below here was copied from the List.java file to remind myself what I need to implement
-* All the script file will need to see is the List.java file
-* LinkedListImpl is an implementation of interface List, using pointers to create the LinkedList
-* Objects are objects in the list. Once an object is in the list it forms a new LinkedList
-*/
-
+ * LinkedListImpl.java is a pointer-based linked list implementation of interface List.java
+ * @author Lburge01 BBK-PiJ-2015-75 (Lucie Burgess)
+ *
+ */
 public class LinkedListImpl implements List {
 	
-	Object obj;
-	LinkedListImpl nextList;
+	private Object obj;
+	private LinkedListImpl nextList;
+	private LinkedListImpl prevList;
+	private int element;
 	
 	/**
 	* Constructor of LinkedListImpl
+	* obj is the first object in the list
+	* nextList is the list (which is empty if obj == null)
 	*/
-	
-	public LinkedListImpl() { // constructor instantiating the class LinkedList
+	public LinkedListImpl() {
+		this.obj = obj;
+		this.nextList = nextList; 
+		this.prevList = prevList;
+		this.element = element;
 	} 	
-	
+
 	/**
+	 * @see List#isEmpty()
 	 * @return true if the list is empty, false otherwise. 
 	 */
-	//@return boolean - need to sort out javadoc comments
-	public boolean isEmpty() { // method isEmpty, does not take parameters, returns a boolean
+	@Override
+	public boolean isEmpty() {
 		if (obj == null) {
 			return true;
 		} else {
 			return false;
 		}
 	}
-
+	
 	/**
+	 * @see List#size()
 	 * @return the number of items currently in the list
 	 */
-	public int size();
+	@Override
+	public int size() {
 		if (obj == null) {
 			return 0;
 		} else if (nextList == null) {
@@ -44,98 +49,102 @@ public class LinkedListImpl implements List {
 			return (1 + nextList.size());
 		} 
 	}
-			
-			
-		
-		
+	
+	
 
-	/**
-	 * Returns the element at the given position. 
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
+	/* (non-Javadoc)
+	 * @see List#get(int)
 	 */
-	public ReturnObject get(int index);
-
-	/**
-	 * Returns the elements at the given position and removes it
-	 * from the list. The indeces of elements after the removed
-	 * element must be updated accordignly.
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * @param index the position in the list of the item to be retrieved
-	 * @return the element or an appropriate error message, 
-	 *         encapsulated in a ReturnObject
-	 */
-	public ReturnObject remove(int index);
-
-	/**
-	 * Adds an element to the list, inserting it at the given
-	 * position. The indeces of elements at and after that position
-	 * must be updated accordignly.
-	 * 
-	 * If the index is negative or greater or equal than the size of
-	 * the list, then an appropriate error must be returned.
-	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
-	 * 
-	 * @param index the position at which the item should be inserted in
-	 *              the list
-	 * @param item the value to insert into the list
-	 * @return an ReturnObject, empty if the operation is successful
-	 *         or containing an appropriate error message otherwise
-	 */
-	public ReturnObject add(int index, Object item) {
+	@Override
+	public ReturnObject get(int index) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
-
+	/* (non-Javadoc)
+	 * @see List#remove(int)
+	 */
+	@Override
+	public ReturnObject remove(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 	/**
-	 * Adds an element at the end of the list.
+	 * @see List#add(int, java.lang.Object)
+	 * Adds an element to the list, inserting it at the given position. 
+	 * The list starts at index 0.
+	 * The indices of elements at and after that position must be updated accordingly.
 	 * 
-	 * If a null object is provided to insert in the list, the
-	 * request must be ignored and an appropriate error must be
-	 * returned.
+	 * If the index is negative or greater or equal than the size of
+	 * the list, then an appropriate error must be returned.
 	 * 
+	 * If a null object is provided to insert in the list, the request must be ignored and an appropriate error must be returned.
+	 * 
+	 * @param index the position at which the item should be inserted in the list
 	 * @param item the value to insert into the list
 	 * @return an ReturnObject, empty if the operation is successful
 	 *         or containing an appropriate error message otherwise
 	 */
-	public ReturnObject add(Object item) { // item is the name of the slot, object is the physical memory we have allocated
 	
+	@Override
+	public ReturnObject add(int index, Object item) {
+		
 		if (item == null) {
-			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT);
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT); // if a null object is provided, @return an appropriate error message
 		}
-	
-		// list is empty
-		if (obj == null) {
+		
+		if (obj == null) { // list is empty
 			obj = item;
-		}
+		
+		} else if (nextList != null) { // i.e. if we are inserting element in the middle of the list
+			LinkedListImpl myList  = new LinkedListImpl();
+			if (index < 0 || index >= (myList.size()-1)) {
+				return new ReturnObjectImpl(ErrorMessage.INDEX_OUT_OF_BOUNDS); // if the index is incorrect @return the appropriate error message
+			} else {
+				// create a new List of the objects up until the index
+				myList = 
+				prevList = myList; // myList is now the previous part of the List
+				myList.nextList = null; //set the pointer of myList to nextList to be null
+				myList.add(item); // add the item using add()
+				
+	
+			}	
 			
-		// we are at the end of the list
-		} else if (nextList == null) {
-			linkedList l  = new linkedList();
-			l.add(item);
-			nextList = l;	
-		// add an item to the list (because the objects themselves don't have pointers)
+		}
+		return new ReturnObjectImpl(null); // @return a ReturnObject, empty if the operation is successful
+	}
+
+	/**
+	 * @see List#add(java.lang.Object)
+	 * Adds an element at the end of the list.
+	 * 
+	 * If a null object is provided to insert in the list, the request must be ignored and an appropriate error must be returned.
+	 * If the list is empty, then obj is the item to be inserted
+	 * or if nextList is null, create a nextList called myList
+	 * or if nextList is not null (already has objects in it), add the item to the list 
+	 * 
+	 * @param item is the value to insert into the list
+	 * @return an ReturnObject, empty if the operation is successful
+	 *         or containing an appropriate error message otherwise (because this list cannot contain null objects)
+	 */
+	@Override
+	public ReturnObject add(Object item) {
+		
+		if (item == null) { // if we try to add a null item to the list
+			return new ReturnObjectImpl(ErrorMessage.INVALID_ARGUMENT); // @return a ReturnObject containing an appropriate error message
+		}
+		
+		if (obj == null) { // list is empty
+			obj = item;
+		} else if (nextList == null) { // if we are at end of list
+			LinkedListImpl myList  = new LinkedListImpl();
+			myList.add(item);
+			nextList = myList;	
 		} else {
 			nextList.add(item);
 		}
-		return new ReturnObjectImpl(null);
+		return new ReturnObjectImpl(null); // @return a ReturnObject, empty if the operation is successful
 	}
-		
-			
-		 
-		 
-		
-	
 	
 }
